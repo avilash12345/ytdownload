c218126aafacc58c4d140273bb697fcab138e44d
avilash12345 <avilashjit@gmail.com>
2023-12-04 21:17:09 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+const PORT = process.env.PORT || 4000;
+const fs = require('fs');
+const { dlAudio } = require("youtube-exec");
+
+//middleware
+app.use(cors());
+app.set("view engine", "ejs");
+// For parsing application/json
+app.use(express.json());
+ 
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+//routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.post("/url",async(req, res) => {
+  const inputurl=req.body
+  //console.log(inputurl.inpurl)
+  // Using async/await
+try {
+  await dlAudio({
+    url: inputurl.inpurl,
+    folder:  'downloads', // optional, default: "youtube-exec"
+    filename: Math.random(), // optional, default: video title
+    quality: "best", // or "lowest"; default: "best"
+  });
+  console.log("Audio downloaded successfully! 🔊🎉");
+} catch (err) {
+  console.error("An error occurred:", err.message);
+}
+  res.redirect('/');
+});
+app.listen(PORT, () => {
+  console.log(`Listening on port http://localhost:${PORT}`);
+});
